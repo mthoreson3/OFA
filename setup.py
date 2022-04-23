668c3486acda3613c2dd323ab5a3acf41d80451f
Marcus Thoreson <mthoreson3@gatech.edu>
2022-04-23 03:36:33 +0000

    Fixing setup

--- setup.py
+++ setup.py
@@ -1,2 +1,13 @@
 from setuptools import setup, find_packages
-setup(name='OFA', packages=find_packages())
+setup(
+        name='OFA', 
+        packages=find_packages(
+            include=[
+                'ofa_module',
+                'criterions',
+                'data',
+                'models',
+                'tasks'
+                'utils']
+        )
+)
