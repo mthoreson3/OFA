f1bfb9700728dcf61ddb35c8a7e93553c45f2830
Marcus Thoreson <marcus@dev1.marcus.mylocal>
2022-04-24 03:52:52 -0400

    wait no this is the last one

--- setup.py
+++ setup.py
@@ -9,17 +9,16 @@ found_pkgs = find_packages(exclude=['fairseq', 'fairseq.*', 'ofa_module'])
 
 prefixed_pkgs = [ofa_name+'.'+pkg for pkg in found_pkgs]
 
-pkgs.append(prefixed_pkgs)
-
 pkg_dirs = {ofa_name: '.'}
 
 for i, pkg in enumerate(found_pkgs):
     pkg_dir = './' + pkg.replace('.', '/')
     pkg_dirs[prefixed_pkgs[i]] = pkg_dir
+    pkgs.append(prefixed_pkgs[i])
 
 setup(
         name='OFA',
-        packages=prefixed_pkgs,
+        packages=pkgs,
         package_dir=pkg_dirs,
         package_data={"": ["*.txt", "*.json", "*.bpe"]}
 )
