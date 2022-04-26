445abfe61b8d038ef86729d69ad4fc974829604b
Marcus Thoreson <marcus@dev1.marcus.mylocal>
2022-04-26 13:23:28 -0400

    Adding .gz files to package

--- setup.py
+++ setup.py
@@ -27,6 +27,6 @@ setup(
         author='Junyang Lin and Yang An',
         packages=pkgs,
         package_dir=pkg_dirs,
-        package_data={"": ["*.txt", "*.json", "*.bpe"]},
+        package_data={"": ["*.txt", "*.json", "*.bpe", "*.gz"]},
         zip_safe=False
 )
